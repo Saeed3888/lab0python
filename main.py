4a233304f5b30deebf2035caf6d611301863bf92
Saeed5062 <Ssa5468@psu.edu>
2020-08-26 09:19:27 +0000

    deleting

--- main.py
+++ main.py
@@ -6,9 +6,3 @@ age = int(age)
 
 print("You will be "+ str(age + 1)+ " next year. ")
 
-name = input("Enter your name: ")
-print("Hello, "+ name + "!")
-age = input("Enter your age: ")
-age = int(age)
-
-print("You will be "+ str(age + 1)+ " next year.")
