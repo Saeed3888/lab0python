3b648c4bfc243d8c0ba0eb198edd3835654f9e44
Saeed5062 <Ssa5468@psu.edu>
2020-08-27 17:03:55 +0000

    everything

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-name = input("Enter your name: ")
 
-print("Hello, "+ name + "!")
-age = input("Enter your age: ")
-age = int(age)
 
-print("You will be "+ str(age + 1)+ " next year.")
+temperature = input("Enter temperature in Celsius: ")
+temperature = float(temperature)
+
+print(str(temperature) + chr(176) +" in Celsius is equivalent to "+ str(temperature *1.8+32)+ chr(176) + " Fahrenheit.")
 
