3e6d3115307fe74d7d67d9312c1244c435c412c7
Saeed5062 <Ssa5468@psu.edu>
2020-08-27 17:06:45 +0000

    cc

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 
 
-temperature = input("Enter temperature in Celsius: ")
+temperature = input("Enter temperature in celsius: ")
 temperature = float(temperature)
 
 print(str(temperature) + chr(176) +" in Celsius is equivalent to "+ str(temperature *1.8+32)+ chr(176) + " Fahrenheit.")
